b1671518ba8491de0c5c6380e67dd311a9bc98e7
david <davidwnewman91@gmail.com>
2019-05-16 09:30:44 -0500

    going into Lecture

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,21 @@
-//Prev/next
+//functions of Prev/next
 // edit/delete/new
 import React, {Component} from 'react'
 
 class Buttons extends Component{
+    // constructor(){
+    // }
     
+    // next(data) {
+    //     if (data[i] <data.length-1){
+    //         return data[i]+1;
+    //     }
+    // }
+    // previous(data){
+    //     if (data[i] >data.length-1){
+    //         return data[i]-1;
+    //     }
+    // }
 
     render(){
         return(
@@ -12,7 +24,7 @@ class Buttons extends Component{
                 {/* <button></button>
                 <button></button>
                 <button></button> */}
-                {/* <button onClick={}>Next</button> */}
+                {/* <button onClick={()=> this.next}>Next</button> */}
             </main>
         )
     }
--- src/components/List.js
+++ src/components/List.js
@@ -12,7 +12,7 @@ class List extends Component{
     constructor(){
         super()
         this.state ={
-            
+            i: 0
         }
     }
 
@@ -21,25 +21,25 @@ class List extends Component{
         return(
             <main className='whiteBox'>
                 <section>
-                    {contact[0].id}/{contact.length}
+                    {contact[this.state.i].id}/{contact.length}
                 </section>
                 <section className='Name'>
-                    {contact[0].name.first} {contact[0].name.last}
+                    {contact[this.state.i].name.first} {contact[this.state.i].name.last}
                 </section>
                 <section className='From'>
-                    <strong>From:</strong> {contact[0].city}, {contact[0].country}
+                    <strong>From:</strong> {contact[this.state.i].city}, {contact[this.state.i].country}
                 </section>
                 <section className='Title'>
-                    <strong>Job Title:</strong> {contact[0].title}
+                    <strong>Job Title:</strong> {contact[this.state.i].title}
                 </section>
                 <section className='Employer'>
-                    <strong>Employer:</strong> {contact[0].employer}
+                    <strong>Employer:</strong> {contact[this.state.i].employer}
                 </section>
                 <section className='Movies'>
                 <strong>Favorite Movies:</strong> <ol>
-                    <li>{contact[0].favoriteMovies[0]}</li>
-                    <li>{contact[0].favoriteMovies[1]}</li>
-                    <li>{contact[0].favoriteMovies[2]}</li></ol>
+                    <li>{contact[this.state.i].favoriteMovies[0]}</li>
+                    <li>{contact[this.state.i].favoriteMovies[1]}</li>
+                    <li>{contact[this.state.i].favoriteMovies[2]}</li></ol>
                 </section>
             </main>
         )
